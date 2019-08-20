c066de6fbfd507e9a669f40f3ac4ea786a0517e5
rainy-me <github@rainy.me>
2019-08-21 02:54:38 +0900

    animate color

--- src/app.cpp
+++ src/app.cpp
@@ -67,6 +67,8 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    glfwSwapInterval(1);
+
     if (glewInit() != GLEW_OK)
     {
         /* Problem: glewInit failed, something is seriously wrong. */
@@ -114,14 +116,21 @@ int main(void)
 
         layout(location = 0) out vec4 color;
 
+        uniform vec4 u_Color;
+
         void main()
         {
-            color = vec4(1.0,0.0,0.0,1.0);
+            color = u_Color;
         }
     )";
 
     auto shader = CreateShader(vertexShader,fragShader);
     glUseProgram(shader);
+
+    int location = glGetUniformLocation(shader, "u_Color");
+    glUniform4f(location,0.2f,0.3f,0.8f,1.0f);
+    float r = 0.0f;
+    float increment = 0.05f;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
@@ -135,8 +144,15 @@ int main(void)
             glVertex2f( 0.5f,-0.5f);
             glEnd();
         */
+       
+        glUniform4f(location,r,0.3f,0.8f,1.0f);
         glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,nullptr);
 
+        if (r> 1.0f)
+            increment = -0.05f;
+        else if (r < 0.0f)
+            increment = 0.05f;
+        r += increment;
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
