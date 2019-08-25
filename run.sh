a00c97f42d029a052f936b70ac6cea66f65dd54f
rainy-me <github@rainy.me>
2019-08-26 04:24:04 +0900

    modify run script

--- run.sh
+++ run.sh
@@ -1,2 +1,16 @@
-g++ src/*.cpp -o bin/app -L /usr/local/lib64  -lGL -lglfw -lGLEW
-MESA_GL_VERSION_OVERRIDE=4.4 LD_LIBRARY_PATH=/usr/local/lib64 ./bin/app
+#!/bin/bash
+
+build(){
+    g++ src/*.cpp -o bin/app -L /usr/local/lib64  -lGL -lglfw -lGLEW
+}
+
+run(){
+    MESA_GL_VERSION_OVERRIDE=4.4 LD_LIBRARY_PATH=/usr/local/lib64 ./bin/app
+}
+
+
+case $1 in 
+    "build") build;;
+    "run") run;;
+    *) build && run;;
+esac
