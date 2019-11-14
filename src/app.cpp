a5f4fe847611ac207c81a73f7108f68b5aa3d659
rainy-me <github@rainy.me>
2019-11-15 05:38:42 +0900

    combine imgui with glm

--- src/app.cpp
+++ src/app.cpp
@@ -28,7 +28,7 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(1000, 500, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -49,10 +49,10 @@ int main(void)
     std::cout << glGetString(GL_VERSION) << std::endl;
 
     float positions[] = {
-        -0.5f,-0.5f, 0.0f, 0.0f,
-         0.5f,-0.5f, 1.0f, 0.0f,
-         0.5f, 0.5f, 1.0f, 1.0f,
-        -0.5f, 0.5f, 0.0f, 1.0f
+        100.0f, 100.0f, 0.0f, 0.0f,
+        200.0f, 100.0f, 1.0f, 0.0f,
+        200.0f, 200.0f, 1.0f, 1.0f,
+        100.0f, 200.0f, 0.0f, 1.0f
     };
     unsigned int indices[] = {
         0, 1, 2,
@@ -72,14 +72,14 @@ int main(void)
 
     IndexBuffer ib(indices, 6);
 
-    glm::mat4 proj = glm::ortho(-0.5f, 0.5f, -1.0f, 1.0f, -1.0f, 1.0f);
+    glm::mat4 proj = glm::ortho(0.0f, 500.0f, 0.0f, 500.0f, -1.0f, 1.0f);
+    glm::mat4 view = glm::translate(glm::mat4(1.0f),glm::vec3(-100, 0, 0));
 
     Shader shader("./shader/logo.glsl");
     Renderer renderer;
 
     // shader.SetUniform4f("u_Color",0.2f,0.3f,0.8f,1.0f);
-    shader.SetUniformMat4f("u_MVP",proj);
-    
+
     Texture texture("./texture/opengl.png");
     shader.SetUniform1i("u_Texture", 0);
 
@@ -93,6 +93,8 @@ int main(void)
     bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
+    glm::vec3 translation(200, 200, 0);
+
     float r = 0.0f;
     float increment = 0.05f;
     /* Loop until the user closes the window */
@@ -105,6 +107,11 @@ int main(void)
         ImGui::NewFrame();
 
         //shader.SetUniform4f("u_Color",r,0.3f,0.8f,1.0f);
+
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+        glm::mat4 mvp = proj * view * model;
+        shader.SetUniformMat4f("u_MVP", mvp);
+    
         renderer.Draw(va, ib, shader);
 
         if (r> 1.0f)
@@ -118,19 +125,7 @@ int main(void)
             static int counter = 0;
 
             ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
+            ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 1000.0f);
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
             ImGui::End();
         }
